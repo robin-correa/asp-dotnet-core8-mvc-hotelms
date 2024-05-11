53b0b05cfefdc2d64e90ef7958b0afa1f3edb152
robin.correa <robin.correa21@gmail.com>
2024-05-11 11:26:11 +0800

    Create initial entities and add required nuget packages.

---
+++ HotelMS.Domain/Entities/RoomCategory.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelMS.Domain.Entities
+{
+    public class RoomCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int TotalCapacity { get; set; }
+        public int TotalAdult { get; set; }
+        public int TotalChild { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
